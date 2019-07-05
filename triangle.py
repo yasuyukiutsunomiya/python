f049d7a52d7dac9dc0882e5654453c3b9cce660e
yasuyukiutsunomiya <csya18003@g.nihon-u.ac.jp>
2019-07-05 13:17:11 +0900

    triangle_1

---
+++ triangle.py
@@ -0,0 +1,16 @@
+for x in range(10):
+	for i in range(x,10):
+		print('*',end='')
+	print('')
+
+
+for i in range(10):
+	for j in range(0,10 - i):
+		print(end=' ')
+	for n in range(0,0 + i):
+		print(end=' ')
+	for m in range(0 + i,10):
+		print('*',end=' ')
+	for k in range(10 - i,10):
+		print('*',end=' ')
+	print('')
